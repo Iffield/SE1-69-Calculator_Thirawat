40802478f783baa2520d256e2e049986f7d99fb1
Iffield <gog75506@gmail.com>
2026-09-15 10:14:29 +0700

    Add file calculator.py

---
+++ calculator.py
@@ -0,0 +1,38 @@
+def add(a, b):
+    return a + b
+
+def subtract(a, b):
+    return a - b
+
+def multiply(a, b):
+    return a * b
+
+def divide(a, b):
+    if b == 0:
+        return "Error: Cannot divide by zero"
+    return a / b
+
+
+expression = input("Enter calculation (example: 10 + 5): ")
+
+num1, operator, num2 = expression.split()
+
+num1 = float(num1)
+num2 = float(num2)
+
+if operator == "+":
+    result = add(num1, num2)
+
+elif operator == "-":
+    result = subtract(num1, num2)
+
+elif operator == "*":
+    result = multiply(num1, num2)
+
+elif operator == "/":
+    result = divide(num1, num2)
+
+else:
+    result = "Invalid operator"
+
+print("Result =", result)
